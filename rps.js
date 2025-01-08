8883bb66898408d6ecd4c1c1568180867cc38117
chelseawinik <chelsea.semou@ada.ac.uk>
2025-01-08 14:19:41 +0000

    Gets rules of the game

--- rps.js
+++ rps.js
@@ -1,7 +1,22 @@
 function rockPaperScissors(player1, player2) {
 
+  if (player1 == player2) {
+    return "draw";
+     // Determine the winner based on the rules of the game
+
+  }
   
+   // Determine the winner based on the rules of the game
    
+   if (
+    (player1 === "rock" && player2 === "scissors") ||
+    (player1 === "scissors" && player2 === "paper") ||
+    (player1 === "paper" && player2 === "rock")
+  ) {
+    return "player1";
+  } else {
+    return "player2";
+  } 
 }
 
 
